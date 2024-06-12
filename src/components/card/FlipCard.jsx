f79ee8f7b17830d7f528adbe1251fcce7201f873
daviamr <daviamorim_sz@live.com>
2024-06-12 16:25:59 -0300

    ajustes na animação flip

--- src/components/card/FlipCard.jsx
+++ src/components/card/FlipCard.jsx
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './FlipCard.css';
 
-export const FlipCard = ({text, path, info}) => {
+export const FlipCard = ({ text, path, info }) => {
   const [flipped, setFlipped] = useState(false);
+  const cardInnerRef = useRef(null);
+  const imgRef = useRef(null);
 
-  const handleClick = () => {
-    setFlipped(!flipped);
+  const handleMouseEnter = () => {
+    setFlipped(true);
   };
 
+  const handleMouseLeave = () => {
+    setFlipped(false);
+  };
+
+  useEffect(() => {
+    const handleImageLoad = () => {
+      if (imgRef.current && cardInnerRef.current) {
+        cardInnerRef.current.style.height = `${imgRef.current.offsetHeight}px`;
+      }
+    };
+
+    const img = imgRef.current;
+    if (img) {
+      if (img.complete) {
+        handleImageLoad();
+      } else {
+        img.addEventListener('load', handleImageLoad);
+      }
+    }
+
+    return () => {
+      if (img) {
+        img.removeEventListener('load', handleImageLoad);
+      }
+    };
+  }, [path]);
+
   return (
-    <div className={`flip-card ${flipped ? 'flipped' : ''}`} onClick={handleClick}>
-      <div className="flip-card-inner">
+    <div className={`flip-card ${flipped ? 'flipped' : ''}`} onMouseEnter={handleMouseEnter} 
+    onMouseLeave={handleMouseLeave}>
+      <div className="flip-card-inner" ref={cardInnerRef}>
         <div className="flip-card-front">
-          <img className="flip-img" src={path} alt={info} />
+          <img className="flip-img" src={path} alt={info} ref={imgRef} />
         </div>
         <div className="flip-card-back">
           <h2 className="flip-text">{text}</h2>
