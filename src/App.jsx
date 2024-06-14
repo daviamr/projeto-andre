ade3b9a453ff0f23ffe5b141af260ec5c5990aba
daviamr <daviamorim_sz@live.com>
2024-06-14 17:38:54 -0300

    feat anchor

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,14 @@ export function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState('');
   const [modalTitleContent, setModalTitleContent] = useState ('');
+  const [modalLinkContent, setModalLinkContent] = useState ('');
 
   const cardInfo = [{
     title: 'Account Creator',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: accountcreator,
+    anchor: '#',
     info: 'Logo Account Creator',
     highlights: ['']
   },
@@ -39,12 +41,14 @@ export function App() {
     modalTextContent: 'Os arquivos com os números para verificação devem ser carregados na plataforma via planilhas (em .XLSX ou .CSV) com até 100.000 linhas. Serviço com capacidade de processar e verificar até 60.000 números por hora.',
     highlights: ['número de telefone'],
     path: checkoperadora,
+    anchor: '#',
     info: 'Logo Check Operadora'
   },
   {title: 'Chip Control',
     text: 'Plataforma para monitoramento e controle de dispositivos chipados, como smartphones e modens.',
     modalTextContent: 'A plataforma verifica: se o dispositivo está respondendo ou não, se o chip ou dispositivo está com os serviços de mensageria via SMS, WhatsApp e Telegram ativos e se as contas de serviços associados ao cadastro do chip, como GMAIL, Yahoo Mail e MSN Outlook estão desbloqueados. Controla versionamento de softwares e aplicativos, além de fazer a gestão dos créditos e recargas de cada chip.',
     path: chipcontrol,
+    anchor: '#',
     info: 'Logo Chip Control',
     highlights: ['dispositivos chipados']
   },
@@ -53,6 +57,7 @@ export function App() {
     text: 'Plataforma para verificação de integridade de listas de e-mails.',
     modalTextContent: 'A solução verifica: se o domínio associado à conta existe e está ativo, se a TLD é válida, se o endereço de email é válido ou não, se a conta de email está ativa e apta a receber mensagens e permite o disparo de uma mensagem-teste.\n\nCorrige automaticamente erros comuns de digitação e sintaxe. Tem como objetivo reduzir a taxa de bounce de listas de disparo de email e com isso melhorar o score dos respectivos remetentes.\n\nOs arquivos com os endereços de email para verificação devem ser carregados na plataforma via planilhas (em .XLSX ou .CSV) com até 100.000 linhas. Serviço com capacidade de processar e verificar até 120.000 e-mails por hora.',
     path: mailchecker,
+    anchor: '#',
     info: 'Logo Mail Checker',
     highlights: ['domínio', 'TLD', 'endereço de email', 'conta de email']
   },
@@ -61,6 +66,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: mailwarmer,
+    anchor: '#',
     info: 'Logo Mail Warmer',
     highlights: ['']
   },
@@ -69,6 +75,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: megabase,
+    anchor: '#',
     info: 'Logo Mega Base',
     highlights: ['']
   },
@@ -77,6 +84,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: megafeed,
+    anchor: '#',
     info: 'Logo Mega Feed',
     highlights: ['']
   },
@@ -85,6 +93,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: megalead,
+    anchor: '#',
     info: 'Logo Mega Lead',
     highlights: ['']
   },
@@ -93,6 +102,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: scrapsender,
+    anchor: '#',
     info: 'Logo Super Sender',
     highlights: ['']
   },
@@ -100,6 +110,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: superscrapper,
+    anchor: '#',
     info: 'Logo Super Scrapper',
     highlights: ['']
   },
@@ -108,6 +119,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: superseller,
+    anchor: '#',
     info: 'Logo Super Seller',
     highlights: ['']
   },
@@ -116,6 +128,7 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: supersender,
+    anchor: '#',
     info: 'Logo Super Sender',
     highlights: ['']
   },
@@ -124,6 +137,7 @@ export function App() {
     text: 'Plataforma que verifica se um número de telefone tem uma conta de WhatsApp associada a ele. Em caso positivo, salva as informações disponíveis, de acordo com as configurações de privacidade de cada usuário.',
     modalTextContent: 'Dados verificados: WhatsApp (Sim ou Não), tipo (Business ou Individual), nome do titular, foto, mensagem de status. Quando a conta verificada for modo "Business", salva também: ramo de atividade, website, email e endereço.\n\nOs arquivos com os números para verificação devem ser carregados na plataforma via planilhas (em .XLSX ou .CSV) com até 100.000 linhas. Serviço com capacidade de processar e verificar até 20.000 números por hora.',
     path: zapchecker,
+    anchor: '#',
     info: 'Logo Zap Checker',
     highlights: ['número de telefone']
   },
@@ -132,13 +146,15 @@ export function App() {
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam accusamus repellendus quo eligendi sequi quae ullam amet voluptate id doloremque facere aliquid, porro rerum blanditiis ut velit? Ratione, veritatis! Odit?',
     modalTextContent: '',
     path: zapwarmer,
+    anchor: '#',
     info: 'Zap Warmer',
     highlights: ['']
   }];
 
-  const handleOpenModal = (content, titleContent) => {
+  const handleOpenModal = (content, titleContent, linkContent) => {
     setModalContent(content);
     setModalTitleContent(titleContent);
+    setModalLinkContent(linkContent);
     setIsModalOpen(true);
   };
 
@@ -146,6 +162,7 @@ export function App() {
     setIsModalOpen(false);
     setModalContent('');
     setModalTitleContent('');
+    setModalLinkContent('');
   };
 
   const formatTextWithLineBreaks = (text) => {
@@ -173,14 +190,15 @@ export function App() {
                 info={item.info}
                 text={item.text}
                 highlights={item.highlights}
-                onButtonClick={() => handleOpenModal(cardInfo[index].modalTextContent, cardInfo[index].title)}/>
+                onButtonClick={() => handleOpenModal(cardInfo[index].modalTextContent, cardInfo[index].title, cardInfo[index].anchor)}/>
               )})
           }
           <CardModal
               isOpen={isModalOpen}
               onClose={handleCloseModal}
               modalTitle={modalTitleContent}
-              content={formatTextWithLineBreaks(modalContent)}>
+              content={formatTextWithLineBreaks(modalContent)}
+              modalLink={modalLinkContent}>
               </CardModal>
         </div>
       </div>
--- src/components/card-modal/CardModal.jsx
+++ src/components/card-modal/CardModal.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from './CardModal.module.css'
 
-export const CardModal = ({isOpen, onClose, children, modalTitle, content}) => {
+export const CardModal = ({isOpen, onClose, children, modalTitle, content, modalLink}) => {
     if (!isOpen) return null;
     
     return (
@@ -9,6 +9,7 @@ export const CardModal = ({isOpen, onClose, children, modalTitle, content}) => {
             <div className={styles.modal}>
                 <h1 className={styles.title}>{modalTitle}</h1>
                 <p className={styles.content}>{content}</p>
+                <a className={styles.anchor} href={modalLink} target='_blank'>Link</a>
                 <button className={styles.modalCloseButton} onClick={onClose}>X</button> {children}
             </div>
         </div>
